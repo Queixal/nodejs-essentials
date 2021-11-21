379f248eee29ebf0fc017ddd60cc75e5082c78d8
Queixal <mmi.developer@gmail.com>
2021-11-21 18:29:01 +0100

    [MINOR] updated templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,16 +2,20 @@
 const path = require('path')
 //load express server function
 const express = require('express')
+//load hbs framework (template)
+const hbs = require('hbs')
 //launch express server
 const app = express()
 //store where templates folder is
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 //store where static fields are stored in
 const staticPath = path.join(__dirname, '../public')
 
 //setup  handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 //global variables from node
 console.log("Node __dirname: " + __dirname)
